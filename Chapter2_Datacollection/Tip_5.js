99c66729e101359e542148b09645deaa13f12622
archepro84 <archepro84@gmail.com>
2021-09-29 01:11:16 +0900

    2021-09-29

---
+++ Chapter2_Datacollection/Tip_5.js
@@ -0,0 +1,72 @@
+function fn_1() {
+    const staff = [
+        {
+            name: 'Wesley',
+            position: 'musician'
+        },
+        {
+            name: 'Davis',
+            position: 'engineer'
+        },
+    ];
+
+    function getMusicians(staff) {
+        return staff.filter(member => member.position === 'musician');
+    }
+
+    console.log(getMusicians(staff));
+}
+
+function fn_2() {
+    const game1 = {
+        player: 'Jim Jonas',
+        hits: 2,
+        runs: 1,
+        errors: 0,
+    };
+
+    const game2 = {
+        player: 'Jim Jonas',
+        hits: 3,
+        runs: 0,
+        errors: 1,
+    };
+
+    const total = {};
+    const stats = Object.keys(game1);
+    for (let i = 0; i < stats.length; i++) {
+        const stat = stats[i];
+        if (stat !== 'player') {
+            total[stat] = game1[stat] + game2[stat];
+        }
+    }
+    console.log(total);
+}
+
+function fn_3() {
+    const dog = {
+        name: 'Don',
+        color: 'black',
+    };
+    console.log(dog.name);
+}
+
+function fn_4() {
+    const dogPair = [
+        ['name', 'Don'],
+        ['color', 'black'],
+    ];
+
+    function getName(dog) {
+        return dog.find(attribute => {
+            return attribute[0] === 'name';
+        })[1];
+    }
+
+    console.log(getName(dogPair));
+}
+
+// fn_1();
+// fn_2();
+// fn_3();
+fn_4();
